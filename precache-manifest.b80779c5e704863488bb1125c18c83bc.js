5df5e2c9f0d52e7503e1e6a468713eda276c95fd
mohak92 <tamhane.mohak@gmail.com>
2020-01-20 19:22:07 -0500

    Updates

---
+++ precache-manifest.b80779c5e704863488bb1125c18c83bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391ac14a934adb1e1cb7d4d7183f23b5",
+    "url": "/the_venue/index.html"
+  },
+  {
+    "revision": "a3b6a4d45aba3b3e87d4",
+    "url": "/the_venue/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4956a88cfd35d489e2f9",
+    "url": "/the_venue/static/js/2.98768940.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/the_venue/static/js/2.98768940.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3b6a4d45aba3b3e87d4",
+    "url": "/the_venue/static/js/main.ad947b19.chunk.js"
+  },
+  {
+    "revision": "323b0b5a81f6cb33ccff",
+    "url": "/the_venue/static/js/runtime-main.c247d0ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the_venue/static/media/logo.5d5d9eef.svg"
+  }
+]);
