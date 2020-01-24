95e1fc5a79a75e12b6d287e404116bc534a995f0
mohak92 <tamhane.mohak@gmail.com>
2020-01-23 23:43:19 -0500

    toggleDrawer open close state init

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrwaer from './SideDrawer';
+
 class Header extends React.Component {
+
+    state = {
+        drawerOpen: false
+    }
+
+    toggleDrawer = (value) => {
+        this.setState({
+            drawerOpen: value
+        });
+    }
+
     render() {
         return (
             <AppBar 
@@ -21,6 +34,18 @@ class Header extends React.Component {
                         <div className="font_righteous header_logo_venue">The Venue</div>
                         <div className="header_logo_title">Musical Events</div>
                    </div>
+
+                   <IconButton
+                        aria-label="Menu"
+                        color="inherit"
+                        onClick={()=> this.toggleDrawer(true)}
+                   >
+                       <MenuIcon />
+                   </IconButton>
+                   <SideDrwaer 
+                    open={this.state.drawerOpen}
+                    onClose={(value)=> this.toggleDrawer(value)}
+                   />
                </Toolbar>
             </AppBar>
         );
